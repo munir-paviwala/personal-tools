00fae7ecc1a536cb3c7e4c4afbdcfa7ad4acdb0c
Munirmahedi Paviwala <munir1612@gmail.com>
2026-06-07 06:20:03 +0530

    refactor: remove background-remover tool and associated testing scripts

--- test-esm.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-fetch('https://esm.sh/@imgly/background-removal@1.4.5')
-  .then(res => res.text())
-  .then(text => console.log(text.substring(0, 300)));
--- test-imgly.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-fetch('https://unpkg.com/@imgly/background-removal@1.4.5/dist/index.mjs')
-  .then(res => res.text())
-  .then(text => console.log(text.substring(0, 300)));
